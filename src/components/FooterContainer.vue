ed18eee6d7ded22fa3b1fcc965d8ba96b5fbf971
Alex Petrenko <alex.petrenko@sigma.software>
2018-09-24 10:15:56 +0300

    FI-1. Create image generator page.

---
+++ src/components/FooterContainer.vue
@@ -0,0 +1,97 @@
+<template>
+    <div>
+        <el-footer>
+            <a href="mailto:alex.petrenko.inbox@gmail.com">© Alexey Petrenko</a> |
+            <a href="#" @click.prevent="dialogVisible = true">{{ $t('text.licence')}}</a>
+        </el-footer>
+        <el-dialog
+          title="Licence"
+          :visible.sync="dialogVisible"
+          width="80%"
+        >
+            <licence></licence>
+            <span
+              slot="footer"
+              class="dialog-footer"
+            >
+            <el-button
+              type="primary"
+              @click="dialogVisible = false"
+            >
+                {{ $t('button.close') }}
+            </el-button>
+          </span>
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+  import Licence from './Licence.vue';
+
+  export default {
+    /**
+     * Name.
+     */
+    name: 'FooterContainer',
+
+    /**
+     * Mixins.
+     */
+    mixins: [],
+
+    /**
+     * Props.
+     */
+    props: {},
+
+    /**
+     * Reactive properties.
+     * @returns {{}}
+     */
+    data () {
+      return {
+        dialogVisible: false,
+      };
+    },
+
+    /**
+     * Created hook.
+     */
+    created () {
+    },
+
+    /**
+     * Mounted hook.
+     */
+    mounted () {
+    },
+
+    /**
+     * Destroyed hook.
+     */
+    destroyed () {
+    },
+
+    /**
+     * Components.
+     */
+    components: {
+      Licence
+    },
+
+    /**
+     * Computed properties.
+     */
+    computed: {},
+
+    /**
+     * Methods.
+     */
+    methods: {},
+
+    /**
+     * Watchers.
+     */
+    watch: {}
+  };
+</script>
